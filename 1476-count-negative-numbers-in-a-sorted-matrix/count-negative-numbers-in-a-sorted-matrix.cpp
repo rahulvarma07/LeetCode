921effa78729609df88c409f0e7580785ed45c86
rahulvarma07 <143200717+rahulvarma07@users.noreply.github.com>
2024-02-27 20:26:57 +0530

    Time: 7 ms (95.79%) | Memory: 12.8 MB (48.24%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,50 +1,29 @@
-// class Solution {
-// public:
-//     int countNegatives(vector<vector<int>>& grid) 
-//     {
-//         int left = 0;
-//         int right = grid.size()-1;
-//         int count = 0;
-//         int mid = 0;
-//         for(int i=0;i<grid.size();i++)
-//         {
-//             left = 0;
-//             right = grid.size();
-//             while(left < right)
-//             {
-//             mid = (left + right)/2;
-//             if(grid[i][mid] >= 0)
-//             {
-//                 left = mid+1;
-//             }
-//             else
-//             {
-//                 right = mid;
-//             }
-//             }
-//             count += (grid[i].size()-left);
-//         }
-//         return count;
-//     }
-// };
-
 class Solution {
 public:
-    int countNegatives(vector<vector<int>>& grid) {
-        int left = 0, right = grid.size() - 1, mid,res=0;
-        for(int i = 0 ; i < grid.size() ; i++){
-            left = 0, right =  grid[i].size();
-            while(left < right){
-                mid = (left + right)/2;
-                if(grid[i][mid] >= 0){
-                    left = mid + 1;
-                }
-                else{
-                    right = mid;
-                }
+    int countNegatives(vector<vector<int>>& grid) 
+    {
+        int left = 0;
+        int right = grid.size()-1;
+        int count = 0;
+        int mid = 0;
+        for(int i=0;i<grid.size();i++)
+        {
+            left = 0;
+            right = grid[i].size();
+            while(left < right)
+            {
+            mid = (left + right)/2;
+            if(grid[i][mid] >= 0)
+            {
+                left = mid+1;
             }
-            res += grid[i].size() - left;
+            else
+            {
+                right = mid;
+            }
+            }
+            count += (grid[i].size()-left);
         }
-        return res;
+        return count;
     }
 };
