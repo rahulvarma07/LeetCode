13280bcfebf37f0e4b9a79e0ff159b5012905088
rahulvarma07 <143200717+rahulvarma07@users.noreply.github.com>
2024-02-27 20:26:19 +0530

    Time: 8 ms (90.47%) | Memory: 12.8 MB (27.25%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,16 +1,50 @@
+// class Solution {
+// public:
+//     int countNegatives(vector<vector<int>>& grid) 
+//     {
+//         int left = 0;
+//         int right = grid.size()-1;
+//         int count = 0;
+//         int mid = 0;
+//         for(int i=0;i<grid.size();i++)
+//         {
+//             left = 0;
+//             right = grid.size();
+//             while(left < right)
+//             {
+//             mid = (left + right)/2;
+//             if(grid[i][mid] >= 0)
+//             {
+//                 left = mid+1;
+//             }
+//             else
+//             {
+//                 right = mid;
+//             }
+//             }
+//             count += (grid[i].size()-left);
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
-    int countNegatives(vector<vector<int>>& grid) 
-    {
-        // Basic approach
-        int count = 0;
-        for(int i=0;i<grid.size();i++)
-        {
-            for(int j=0;j<grid[i].size();j++)
-            {
-                if(grid[i][j] < 0)count += 1;
+    int countNegatives(vector<vector<int>>& grid) {
+        int left = 0, right = grid.size() - 1, mid,res=0;
+        for(int i = 0 ; i < grid.size() ; i++){
+            left = 0, right =  grid[i].size();
+            while(left < right){
+                mid = (left + right)/2;
+                if(grid[i][mid] >= 0){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid;
+                }
             }
-        } 
-        return count;   
+            res += grid[i].size() - left;
+        }
+        return res;
     }
 };
