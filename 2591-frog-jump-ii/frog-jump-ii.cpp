ba14f9ce9f5557e3266f7b192f41cedb3483b218
rahulvarma07 <143200717+rahulvarma07@users.noreply.github.com>
2024-02-27 21:48:04 +0530

    Time: 107 ms (62.13%) | Memory: 67.7 MB (30.23%) - LeetSync

---
+++ 2591-frog-jump-ii/frog-jump-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxJump(vector<int>& nums) 
+    {
+        if(nums.size() == 2) return nums[1];
+        int left = 0;
+        int right = 2;
+        int maxx = 0;
+        int diff = 0;
+        while(right < nums.size())
+        {
+            diff = nums[right] - nums[left];
+            maxx = max(maxx,diff);
+            right += 1;
+            left += 1;
+            // cout<<left<<" "<<right;
+            // cout<<endl;
+        }
+        return maxx;
+    }
+};
