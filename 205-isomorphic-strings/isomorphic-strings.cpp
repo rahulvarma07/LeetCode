3a38f7ac283bbc7e1dedd5548b0808e3958a3da3
rahulvarma07 <143200717+rahulvarma07@users.noreply.github.com>
2024-04-02 19:13:23 +0530

    Time: 4 ms (78.16%) | Memory: 9.4 MB (42.66%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        
         unordered_map<char, char> mapS2T;
-        unordered_map<char, char> mapT2S;
-        
+        unordered_map<char, char> mapT2S; 
         for (int i = 0; i < s.size(); ++i) {
             char charS = s[i];
             char charT = t[i];
-            
-            // Check if there's a mapping for charS in mapS2T and if it maps to the same character in t
             if (mapS2T.find(charS) != mapS2T.end()) {
                 if (mapS2T[charS] != charT) {
                     return false;
                 }
-            } else { // If no mapping exists, check if charT is already mapped to some other character in mapT2S
+            } 
+            else { 
                 if (mapT2S.find(charT) != mapT2S.end()) {
                     return false;
                 }
@@ -22,7 +19,6 @@ public:
                 mapT2S[charT] = charS;
             }
         }
-        
         return true;
     }
 };
