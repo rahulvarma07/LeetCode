2dcd7d26f76d3977bb0b6177e9b0fe07fd0d1698
rahulvarma07 <143200717+rahulvarma07@users.noreply.github.com>
2024-05-12 18:49:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (32.53%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        /*
+        1. Take three variables to maintain the count.
+        2. Count 0 , 1, and 2
+        3. Rearrange the array according to the count.
+        4. Optimal compared to sorting : Nlogn
+        5. Time complexity 2n.
+        */
         int count0 = 0;
         int count1 = 0;
         int count2 = 0;
@@ -12,8 +19,7 @@ public:
             }
             else if(nums[i] == 1) count1 ++;
             else count2 ++;
-        }
-        cout << count0 << " " << count1 << " " << count2;
+        }// n
     for(int i = 0; i < count0 ; i ++)
     {
         nums[i] = 0;
@@ -25,7 +31,7 @@ public:
     for(int i = count1+count0 ; i < nums.size(); i ++)
     {
         nums[i] = 2;
-    }
+    }//n
 
     }
 };
