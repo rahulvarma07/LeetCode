71b84b56ef3724ad0ba74770e1e437a23c35b797
rahulvarma07 <143200717+rahulvarma07@users.noreply.github.com>
2024-05-13 17:57:31 +0530

    Time: 154 ms (22.42%) | Memory: 126.3 MB (84.59%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-       vector<int>ans(nums.size(), 0);
-       /*
-       My Brute Force approach
-       1. Take an array fix first half with positive and second half with negative.
-       2. And alternatly arrange them
-       */
-       int pi = 0;
-       int ni = nums.size() / 2;
-       for(int i = 0 ; i < nums.size(); i++)
-       {
-         if(nums[i] > 0)
-         {
-            ans[pi] = nums[i];
-            pi += 1;
-         }
-         else 
-         {
-            ans[ni] = nums[i];
-            ni += 1;
-         }
-       }
-       pi = 0;
-       ni = nums.size() / 2;
-       int i = 0;
-       int j = 1;
-       while(j < nums.size())
-       {
-          nums[i] = ans[pi];
-          pi += 1;
-          i += 2;
-          nums[j] = ans[ni];
-          ni += 1;
-          j += 2;
-       }
-       return nums;
+        vector<int>ans(nums.size(), 0);
+        int pi = 0;
+        int ni = 1;
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+            if(nums[i] < 0)
+            {
+                ans[ni] = nums[i];
+                ni += 2;
+            }
+            else 
+            {
+                ans[pi] = nums[i];
+                pi += 2;
+            }
+        }
+    return ans;   
     }
 };
