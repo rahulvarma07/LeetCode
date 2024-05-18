4849cfea4547ac931368e8ba89a6fc29c4adc15d
rahulvarma07 <143200717+rahulvarma07@users.noreply.github.com>
2024-05-18 16:13:57 +0530

    Time: 11 ms (43.03%) | Memory: 7.1 MB (35.02%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fin(int n)
+    {
+        if(n <= 1) return n;
+
+        return fin(n - 1) + fin(n - 2);
+    }
+    int fib(int n) {
+       int ans =  fin(n);
+       return ans;
+    }
+};
