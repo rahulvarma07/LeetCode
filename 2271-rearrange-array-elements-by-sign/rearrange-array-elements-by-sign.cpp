3db84fe00acd5e887f2361562bd6820577284f48
rahulvarma07 <143200717+rahulvarma07@users.noreply.github.com>
2024-05-21 10:04:05 +0530

    Time: 156 ms (16.96%) | Memory: 126.4 MB (63.44%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    vector<int> rearrangeArray(vector<int>& nums) {
-        /*
-        "OPTIMIZED SOLUTION"
-        1. take positive index as "0" and negative index as "1".
-        2. Iterate through nums
-        3. Change values in ans vector..
-        */ 
-        vector<int>ans(nums.size(), 0);
-        int pi = 0;
-        int ni = 1;
-        for(int i = 0 ; i < nums.size(); i++)
+    vector<int> rearrangeArray(vector<int>& nums) 
+    {
+     vector<int>ans(nums.size(), 0);
+     int posind= 0;
+     int negind = 1;
+     for(int i = 0; i < nums.size(); i++)
+     {
+        if(nums[i] < 0)
         {
-            if(nums[i] < 0)
-            {
-                ans[ni] = nums[i];
-                ni += 2;
-            }
-            else 
-            {
-                ans[pi] = nums[i];
-                pi += 2;
-            }
+            ans[negind] = nums[i];
+            negind += 2;
         }
-    return ans;   
+        else
+        {
+            ans[posind] = nums[i];
+            posind += 2;
+        }
+     }   
+     return ans;
     }
 };
