6ec7925a4d294d7043e2902eaf428fca270e49ca
rahulvarma07 <143200717+rahulvarma07@users.noreply.github.com>
2024-05-25 11:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (80.96%) - LeetSync

---
+++ 3221-find-the-peaks/find-the-peaks.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        vector<int>ans;
+        for(int i = 1; i < mountain.size()-1; i++)
+        {
+            if(mountain[i] > mountain[i+1] and mountain[i] > mountain[i-1])
+            {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
