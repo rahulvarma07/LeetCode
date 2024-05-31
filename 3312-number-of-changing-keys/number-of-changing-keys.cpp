6b97f3919d64823f7ab8225e6b7b682f7f97d256
rahulvarma07 <143200717+rahulvarma07@users.noreply.github.com>
2024-05-31 09:59:40 +0530

    Time: 5 ms (26.97%) | Memory: 8 MB (24.41%) - LeetSync

--- 3312-number-of-changing-keys/number-of-changing-keys.cpp
+++ 3312-number-of-changing-keys/number-of-changing-keys.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int countKeyChanges(string s) {
-        for(int i = 0; i < s.size(); i++)
-        {
-            s[i] = tolower(s[i]);
-        }
         int count = 0;
         for(int i = 1; i < s.size(); i++)
         {
-            if(s[i] != s[i-1]) count ++;
+            if(tolower(s[i]) != tolower(s[i-1])) count ++;
         }
         return count;
     }
