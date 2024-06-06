f19675b3d634f02dc9bc61c4728f78e62afea822
rahulvarma07 <143200717+rahulvarma07@users.noreply.github.com>
2024-06-06 12:00:38 +0530

    Time: 50 ms (82.91%) | Memory: 31.3 MB (38.84%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        sort(hand.begin(), hand.end());
+        unordered_map<int,int>mymap;
+        vector<int>p;
+        int count = groupSize;
+        int temp = groupSize;
+        for(auto a:hand){
+            mymap[a] += 1;
+        }
+        for(int i = 0; i < hand.size(); i++){
+            int a = hand[i];
+            while(temp --){
+                if(mymap.find(a) == mymap.end()) break; 
+                if(mymap.find(a) != mymap.end()){
+                    count -= 1;
+                    mymap[a] -= 1;
+                }
+                if(mymap[a] <= 0) mymap.erase(a);
+                a += 1;
+            }
+            if(count != 0 and count != groupSize) return false;
+            p.push_back(count);
+            temp = groupSize;
+            count = groupSize;
+        }
+        return true;
+    }
+};
