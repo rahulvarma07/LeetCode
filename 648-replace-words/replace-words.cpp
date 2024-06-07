56d2ab94580cb277e2f64e442d89197003fda92b
rahulvarma07 <143200717+rahulvarma07@users.noreply.github.com>
2024-06-07 09:21:39 +0530

    Time: 190 ms (37.79%) | Memory: 37.4 MB (91.74%) - LeetSync

---
+++ 648-replace-words/replace-words.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        unordered_map<string, int>ans;
+
+        for(auto a:dictionary){
+            ans[a] += 1;
+        }
+
+        int i = 0;
+        string s = "";
+        string s1 = "";
+
+        while(i < sentence.size()){
+            s += sentence[i];
+            if(sentence[i] == ' '){
+                s1 += s;
+                s = "";
+            }
+            i += 1;
+            if(ans.find(s) != ans.end()){
+                s1 += s;
+                while(sentence[i] >= 'a' and sentence[i] <= 'z'){
+                    i ++;
+                }
+                s ="";
+            }
+        }
+        s1 += s;
+        //cout << s1;
+        return s1;
+    }
+};
