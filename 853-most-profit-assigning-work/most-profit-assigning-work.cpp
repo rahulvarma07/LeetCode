47a511ae69de56ae98ad4537ec453129eb5c73f4
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-06-18 10:17:37 +0530

    Time: 61 ms (39.05%) | Memory: 44.3 MB (25.52%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int maxProfitAssignment(vector<int>& d, vector<int>& p, vector<int>& worker) {
         unordered_map<int, int>check;
+
         for(int i = 0; i < d.size(); i++){
             int maxx = max(p[i], check[d[i]]);
             check[d[i]] = maxx;
@@ -9,19 +10,12 @@ public:
 
         sort(d.begin(), d.end());
 
-        // for(auto a:d){
-        //     cout << a << " ";
-        // }
-        // cout << endl;
-        // for(auto a:d){
-        //     cout << check[a] << " ";
-        // }
-
         for(int i = 1; i < d.size(); i++){
             if((check[d[i]]) <= (check[d[i-1]])){
                 check[d[i]] = check[d[i-1]];
             }
         }
+
         int ans = 0;
         for(int i = 0; i < worker.size(); i++){
             int x = 0;
@@ -41,6 +35,7 @@ public:
                 ans += check[d[ind]];
             }
         }
+        
         return ans;
     }
 };
