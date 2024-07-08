fb75f3825ee744fc4670e63097a1eff2819796a3
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-07-08 05:40:59 +0530

    Time: 4 ms (31.12%) | Memory: 9.7 MB (72.48%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) 
-    {
-        /*
-        Most Optimal Solution ## Dutch Flag Algorithm ##
-        // Example [2, 0 , 2, 1, 1, 0]
-        // take three pointer's low mid and high
-        // place low and mid at 0th index 
-        // place high at n-1 th index
-        */
-        /*
-        the array should be like [0 to low , low to mid , mid to n - 1]
-                                 // 0 //      // 1 //      // 2 //
-        */
-        int low = 0, mid = 0;
-        int high = nums.size() - 1;
-        while(mid <= high)
-        {
-            if(nums[mid] == 0)
-            {
-                swap(nums[mid], nums[low]); // When 0 swap with low 
+    void sortColors(vector<int>& nums) {
+        int low = 0, mid = 0, high = nums.size()-1;
+        while(mid <= high){
+            if(nums[mid] == 1){
+                mid ++;
+            }
+            else if(nums[mid] == 2){
+                swap(nums[mid], nums[high]);
+                high -= 1;
+            }
+            else{
+                swap(nums[mid], nums[low]);
                 low += 1;
                 mid += 1;
-            }
-            else if(nums[mid] == 1)
-            {
-                mid += 1; // when one don't swap but increment the value of mid
-            }
-            else 
-            {
-                swap(nums[mid], nums[high]);
-                high -= 1; // swap with high when 2
             }
         }
     }
