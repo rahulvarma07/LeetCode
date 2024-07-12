f8561ab8ce13bf9449832a5fc206bd7f240e00dc
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-07-12 05:43:13 +0530

    Time: 8 ms (14.71%) | Memory: 15 MB (16.12%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
-    /*
-    1. Solve this problem in three Steps..
-    step - 1 : Find The Breaking Point ie.(if(nums[i] < nums[i+1]))..
-    */
-    void nextPermutation(vector<int>& nums) 
-    {
-        int n = nums.size();
+    void nextPermutation(vector<int>& nums) {
         int ind = -1;
-        for(int i = n - 2; i >= 0; i--)
-        {
-            if(nums[i] < nums[i+1])
-            {
+        int n = nums.size();
+        for(int i = n-2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
                 ind = i;
                 break;
             }
         }
-        if(ind == -1) 
-        {
-            reverse(nums.begin(), nums.end());
+
+        if(ind == -1){
+            sort(nums.begin(), nums.end());
         }
-        else
-        {
-        for(int i = n - 1; i >= ind; i--)
-        {
-            if(nums[i] > nums[ind])
-            {
+
+        else{
+        for(int i = n-1; i >= 0; i--){
+            if(nums[i] > nums[ind]){
                 swap(nums[i], nums[ind]);
                 break;
             }
         }
-        reverse(nums.begin()+ind+1, nums.end());
+        sort(nums.begin()+ind+1, nums.end());
         }
     }
 };
