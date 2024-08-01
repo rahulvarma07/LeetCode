f9c679699c629bc8d6c7b8b626f140b6cb64744c
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-08-01 06:20:09 +0530

    Time: 8 ms (47.01%) | Memory: 17.6 MB (16.71%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int countSeniors(vector<string>& details) {
         int ans = 0;
-        for(int i = 0; i < details.size(); i ++)
-        {
-            int n = ((details[i][11] - '0') * 10) + (details[i][12] - '0');
+        int check = 1;
+        for(int i = 0; i < details.size(); i++){
+            int n = (details[i][11] - '0')*10 + (details[i][12] - '0');
             if(n > 60) ans += 1;
         }
         return ans;
