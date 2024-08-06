25cb040d6cabed429a01ea25bc8d199007a2c787
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-08-06 19:18:18 +0530

    Time: 77 ms (44.95%) | Memory: 27.7 MB (14.43%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        unordered_map<char, int>check;
-        for(auto a:word){
-            check[a] += 1;
+        vector<int>myVec(26, 0);
+        for(auto a: word){
+            myVec[a - 'a'] += 1;
         }
-        vector<pair<int, char>>myVec;
-        for(auto a: check){
-            pair<int, char> myPair = {a.second, a.first};
-            myVec.push_back(myPair);
-        }
-        int ans = 0;
-        int cnt = 0;
-        int res;
         sort(myVec.rbegin(), myVec.rend());
+        int ans = 0;
+        int res;
         for(int i = 0; i < myVec.size(); i++){
             if(i <= 7){
                 res = 1;
-                ans += (myVec[i].first * res);
+                ans += (myVec[i] * res);
             }
             else if(i <= 15){
                 res = 2;
-                ans += (myVec[i].first * res);
+                ans += (myVec[i] * res);
             }
             else if(i <= 23){
                 res = 3;
-                ans += (myVec[i].first * res);
+                ans += (myVec[i] * res);
             }
             else{
                 res = 4;
-                ans += (myVec[i].first * res);
+                ans += (myVec[i] * res);
             }
-            cout << myVec[i].first << " " << res << " |";
         }
         return ans;
     }
