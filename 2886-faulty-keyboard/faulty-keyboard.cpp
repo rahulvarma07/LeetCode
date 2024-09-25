84920b84552f49532c8b4f52d75b3791ff89f546
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-09-25 22:09:16 +0530

    Time: 9 ms (27.98%) | Memory: 9.8 MB (70.93%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string ans = "";
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != 'i'){
+                ans += s[i];
+                continue;
+            }
+            reverse(ans.begin(), ans.end());
+        }
+        return ans;
+    }
+};
