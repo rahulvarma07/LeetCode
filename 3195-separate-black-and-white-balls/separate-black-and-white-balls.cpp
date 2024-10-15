d95279d366ec4fd924e163cd4b47b777f9d2952c
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-10-15 06:46:06 +0530

    Time: 41 ms (10.95%) | Memory: 15.7 MB (45.68%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long int ans = 0;
+        int i = 0, j = 0;
+        while(s[j] == '0' && j < s.size()){
+            j++;
+        }
+        i = j;
+        for( j;j < s.size(); j++){
+            if(s[j] == '0'){
+                ans += (j-i);
+                swap(s[j], s[i]);
+                i++;
+                while(s[i] == '0' && i < s.size()) i++;
+            }
+        }
+        cout << s << endl;
+        return ans;
+    }
+};
