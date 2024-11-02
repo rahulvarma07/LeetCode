f7b3c58374866f9befc1be3f421539eb469fce60
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-11-02 11:53:10 +0530

    Time: 13 ms (33.60%) | Memory: 19.1 MB (8.09%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -2,22 +2,16 @@ class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
         if(nums.size() == 1) return 0;
-        unordered_map<int, int>check;
         sort(nums.begin(), nums.end());
-        int ans = 0;
-        for(auto a: nums) cout << a << " ";
-        cout << endl;
+        unordered_map<int, int>check;
+        set<pair<int,int>> res;
         for(int i = 0; i < nums.size(); i++){
-            int rem = nums[i]-k;
-            if(check.find(rem) != check.end() && check[nums[i]] == 0){
-                ans += 1;
+            int rem = nums[i] - k;
+            if(check.find(rem) != check.end()){
+                res.insert({rem, nums[i]});
             }
             check[nums[i]]++;
         }
-        if(check.size() == 1) return 1;
-        if(k == 0){
-            for(auto a: check) ans += (a.second / 2);
-        }
-        return ans;
+        return res.size();
     }
 };
