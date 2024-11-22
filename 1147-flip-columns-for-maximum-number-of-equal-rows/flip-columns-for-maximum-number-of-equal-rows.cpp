07115dbca91feff3586b3eca933020cc73850f4e
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-11-22 17:47:26 +0530

    Time: 88 ms (28.99%) | Memory: 89.1 MB (7.84%) - LeetSync

---
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        map<vector<int>,int>check;
+        int maxx = 1;
+        for(auto a:matrix){
+            check[a]++;
+        }
+        for(int i = 0; i < matrix.size(); i++){
+            vector<int>ch = matrix[i];
+            for(int j = 0; j < ch.size(); j++){
+                ch[j] = (ch[j]==1)?0:1;
+            }
+            check[ch]++;
+            maxx = max(maxx, check[ch]);
+        }
+        return maxx;
+    }
+};
