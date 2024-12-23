a925ff8e3cfc05950a49560d03e7d3203bf62beb
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-12-24 05:18:12 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (8.87%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-      vector<int>ans(2, -1);
-      int left = 0, right = nums.size() - 1;
-
-      // FIRST OCCURENCE
-      
-      while(left <= right){
-        int mid = (left + right)/2;
-        if(nums[mid] == target){
-            ans[0] = mid; 
-            right = mid - 1;
+        // Lower Bound Upper Bound..
+        vector<int>ans(2, -1);
+        int low = 0, high = nums.size()-1;
+        while(low <= high){
+            int mid = (high + low)/2;
+            if(nums[mid] == target){
+                ans[0] = mid;
+                high = mid-1;
+            }
+            else if(nums[mid] < target) low = mid+1;
+            else high = mid-1;
         }
-        else if(nums[mid] > target){
-            right -= 1;
+        low = 0;
+        high = nums.size()-1;
+        while(low <= high){
+            int mid = (high + low)/2;
+            if(nums[mid] == target){
+                ans[1] = mid;
+                low = mid+1;
+            }
+            else if(nums[mid] < target) low = mid+1;
+            else high = mid-1;
         }
-        else{
-            left = mid + 1;
-        }
-      }
-      left = 0;
-      right = nums.size() - 1;
-
-      //LAST OCCURENCE
-      while(left <= right){
-        int mid = (left + right)/2;
-        if(nums[mid] == target){
-            ans[1] = mid;
-            left = mid + 1;
-        }
-        else if(nums[mid] > target){
-            right -= 1;
-        }
-        else{
-            left = mid + 1;
-        }
-      }
-      return ans;
+        return ans;
     }
 };
