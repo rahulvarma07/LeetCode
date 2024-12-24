6c1de8233ee39fef0c991bfa766282d70e2b9ce3
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-12-24 05:56:41 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (33.23%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n = nums.size();
-
-        // Base Conditions...
-        if(n == 1)return nums[0];
-        if(nums[0] != nums[1]) return nums[0];
-        if(nums[n-1] != nums[n-2]) return nums[n-1];
-        ios_base::sync_with_stdio(0);
-        cin.tie(nullptr);
-        int strt = 1, end = n-2;
-        while(strt <= end){
-            int mid = (strt + end)/2;
-            if(nums[mid] != nums[mid+1] and nums[mid] != nums[mid-1]){
-                return nums[mid];
-            }
-            else if((nums[mid] == nums[mid-1] and mid % 2 != 0) || (mid % 2 == 0 and nums[mid] == nums[mid+1]))
-            {
-                strt = mid + 1;
-            }
-            else end = mid - 1;
+        if(nums.size() == 1) return nums[0];
+        int low = 0, high = nums.size()-1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(mid == 0 && nums[mid+1] != nums[mid]) return nums[0];
+            if(mid == nums.size()-1 && nums[nums.size()-2] != nums[nums.size()-1]) return nums[mid];
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+            else if((nums[mid] == nums[mid+1] && mid%2 != 0) || (nums[mid] == nums[mid-1] && mid%2 == 0)) high = mid-1;
+            else low = mid+1;
         }
-        return 0;
+        return -1;
     }
 };
