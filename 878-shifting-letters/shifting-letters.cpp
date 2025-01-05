ea70388bf6914ec2c9d4eb4a88c007ffb0858dd7
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2025-01-05 11:46:59 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (53.18%) - LeetSync

--- 878-shifting-letters/shifting-letters.cpp
+++ 878-shifting-letters/shifting-letters.cpp
@@ -7,15 +7,11 @@ public:
     }
     string shiftingLetters(string s, vector<int>& shifts) {
         int n = s.size();
-        vector<long long int>pref(n, 0);
-        pref[n-1] = shifts[n-1];
-        for(int i = n-2; i >= 0; i--){
-            pref[i] = pref[i+1]+shifts[i];
+        long long sum = 0;
+        for(int i = n-1; i >= 0; i--){
+            sum += shifts[i];
+            s[i] = shift(s[i], sum);
         }
-        string str = "";
-        for(int i = 0; i < n; i++){
-            str += shift(s[i], pref[i]);
-        }
-        return str;
+        return s;
     }
 };
