100e0889e95b9e4277ed67375662028a89452f9c
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2025-01-22 22:35:47 +0530

    Time: 10 ms (47.45%) | Memory: 77.9 MB (27.11%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,42 +1,20 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        int ans = 0;
-        int n= nums.size();
-        cout<<n<<" length"<<endl;
-        vector<int>check(2, -1);
-        int i = 0, j = 0, x = 0, co = 0, curr = 0;
+    int countLessThanK(vector<int>nums, int k){
+        if(k < 0) return 0;
+        int oddCount = 0, ans = 0, i = 0, j = 0;
         while(j < nums.size()){
-            if(nums[j]%2 != 0){
-                co++;
+            if(nums[j]%2 != 0) oddCount++;
+            while(oddCount > k){
+                if(nums[i]%2 != 0) oddCount--;
+                i++;
             }
-            if(x == 0 && nums[j]%2 != 0){
-                check[x] = j;
-                x++;
-            }
-            while(co > k){
-                cout << check[0] << " " << check[1] << endl;
-                ans += ((j-check[1])*(check[0]-curr+1));
-                // curr++;
-                int f=0;
-                while(nums[i]%2==0 or !f){
-                    if(nums[i]%2){f=1;co--;}
-                    i++;
-                }
-                cout<<ans<<" i "<<i<<endl;
-                curr = check[0]+1;
-                check[0] = i;
-                check[1] = j;
-            }
-            if(co == k && nums[j]%2 != 0){
-                check[1] = j;
-            }
+            ans += (j-i)+1;
             j++;
         }
-        cout << check[0] << " " << check[1]<<" "<<curr;
-        if(check[0] == -1 && check[1] == -1) return 0;
-        if(check[1] == -1) return 0;
-        ans += ((j-check[1])*(check[0]-curr+1));
         return ans;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return countLessThanK(nums, k) - countLessThanK(nums, k-1);
     }
 };
