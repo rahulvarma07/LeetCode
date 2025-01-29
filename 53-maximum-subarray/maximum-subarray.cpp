31b7aedb2d3a854e7cb064d1fad36253b4a3650f
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2025-01-29 19:52:51 +0530

    Time: 1 ms (27.51%) | Memory: 71.7 MB (52.82%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        /*
-        Kadane's Algorithm
-        1. take a sum which adds the elemnt's of the Array
-        2. check for the Below Conditio
-           if(Your sum Sum < "0") 
-           {
-            the initialize the sum to 0 
-           }
-        3. Everytime update the ans by taking the max element of the sum;
-        */
-        int ans = INT_MIN;
-        int sum = 0;
-        for(int i = 0 ; i < nums.size() ; i ++)
-        {
+        int sum = 0, mx = INT_MIN;
+        for(int i = 0; i < nums.size(); i++){
             sum += nums[i];
-            ans = max(ans, sum);
-            if(sum < 0) sum =0 ;
+            mx = max(mx, sum);
+            if(sum < 0) sum = 0;
         }
-        return ans;
+        return mx;
     }
 };
