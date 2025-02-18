f241be82ddf4708813f8d370fb354f532a9225ae
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2025-02-18 10:30:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (94.85%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.cpp
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    void compute(int ind, string str, string &s, vector<string>&ans){
+    void compute(int ind, string &s, vector<string>&ans){
         if(ind == s.size()){
             ans.push_back(s);
             return;
         }
         if(s[ind] >= '0' && s[ind] <= '9'){
-            compute(ind+1, str, s, ans);
+            compute(ind+1, s, ans);
             return;
         }
         s[ind] = toupper(s[ind]);
-        compute(ind+1, str, s, ans);
+        compute(ind+1, s, ans);
         s[ind] = tolower(s[ind]);
-        compute(ind+1, str, s, ans);
+        compute(ind+1, s, ans);
     }
     vector<string> letterCasePermutation(string s) {
         vector<string>ans;
-        string str = "";
-        compute(0, str, s, ans);
+        compute(0,s, ans);
         return ans;
     }
 };
