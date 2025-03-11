a61d7d2678225f07db1c14dec50d4b84d2127374
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2025-03-11 09:31:07 +0530

    Time: 42 ms (10.92%) | Memory: 16.8 MB (8.98%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
+        int i = 0, j = 0;
         unordered_map<char,int>check;
-        int i = 0, j = 0, ans = 0, n = s.size();
+        int ans = 0;
         while(j < s.size()){
             check[s[j]]++;
             while(check.size() == 3){
-                ans += (n-j);
+                ans += (s.size()-j);
                 check[s[i]]--;
                 if(check[s[i]] == 0) check.erase(s[i]);
                 i++;
             }
             j++;
         }
-        cout << i;
-        // if(i == 0 && check.size() == 3) return 1;
         return ans;
     }
 };
