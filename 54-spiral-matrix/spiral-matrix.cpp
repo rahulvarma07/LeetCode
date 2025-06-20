eaf1d8fd3991f8566cfb602d68f0752de8ac05da
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2025-06-20 12:13:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (13.73%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int left = 0, right = m - 1, top = 0, bottom = n - 1;
+        int n = matrix.size(), m = matrix[0].size();
+        int left = 0, top = 0, right = m-1, bottom = n-1;
         vector<int>ans;
-        while(left <= right and top <= bottom )
-        {
-        for(int i = left; i <= right; i++)
-        {
-            ans.push_back(matrix[left][i]);
-        }
-        top += 1;
-        for(int i = top; i <= bottom; i++)
-        {
-            ans.push_back(matrix[i][right]);
-        }
-        right -= 1;
-        if(top <= bottom)
-        {
-            for(int i = right; i >= left; i--)
-            {
-                ans.push_back(matrix[bottom][i]);
+        while(left <= right && top <= bottom){
+            for(int i = left; i <= right; i++){
+                ans.push_back(matrix[top][i]);
             }
-        bottom --;
-        }
-        if(left <= right)
-        {
-            for(int i = bottom; i >= top; i--)
-            {
-                ans.push_back(matrix[i][left]);
+            top++;
+            for(int i = top; i <= bottom; i++){
+                ans.push_back(matrix[i][right]);
             }
-         left += 1;
-        }
+            right--;
+            if(top <= bottom){
+                for(int i = right; i >= left; i--){
+                    ans.push_back(matrix[bottom][i]);
+                }
+            }
+            bottom--;
+            if(left <= right){
+                for(int i = bottom; i >= top; i--){
+                    ans.push_back(matrix[i][left]);
+                }
+            }
+            left++;
         }
         return ans;
     }
