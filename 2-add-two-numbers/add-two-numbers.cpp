a632f7d203c532e0d6bc63cb1fc57db3ed2fa143
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2025-07-15 21:03:33 +0530

    Time: 2 ms (39.30%) | Memory: 77.1 MB (72.94%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,45 +11,25 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* temp1 = l1;
-        ListNode* temp2 = l2;
-        ListNode* dummy = new ListNode(0);
-        ListNode* gen = dummy;
-        int sum = 0, carry = 0;
-        while(temp1 != NULL && temp2 != NULL){
-            sum = (temp1->val + temp2->val);
+        ListNode* ans = new ListNode(-1);
+        ListNode* temp = ans;
+        int carry = 0;
+        while((l1 != NULL || l2 != NULL) || carry){
+            int sum = 0;
+            if(l1){
+                sum += l1->val;
+                l1 = l1->next;
+            } 
+            if(l2){
+                sum += l2->val;
+                l2 = l2->next;
+            }
             sum += carry;
-            temp1 = temp1->next;
-            temp2 = temp2->next;
-            ListNode* Val = new ListNode(sum % 10);
             carry = sum/10;
-            gen->next = Val;
-            gen = gen->next;
-        }
-        while(temp1 != NULL){
-            sum = temp1->val;
-            sum += carry;
-            temp1 = temp1->next;
-            ListNode* Val = new ListNode(sum % 10);
-            carry = sum/10;
-            gen->next = Val;
-            gen = gen->next;
-        }
-        while(temp2 != NULL){
-            sum = temp2->val;
-            sum += carry;
-            temp2 = temp2->next;
-            ListNode* Val = new ListNode(sum % 10);
-            carry = sum/10;
-            gen->next = Val;
-            gen = gen->next;
-        }
-        
-        if(carry != 0){
-            ListNode* Val = new ListNode(carry);
-            gen->next = Val;
-        }
-
-        return dummy->next;
+            ListNode* curr = new ListNode(sum%10);
+            temp->next = curr;
+            temp = curr;
+        }         
+        return ans->next;
     }
 };
