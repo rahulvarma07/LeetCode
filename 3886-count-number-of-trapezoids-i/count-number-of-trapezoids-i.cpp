7334062379b47513aebfc8c07096bb4310999adb
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2025-07-20 22:30:49 +0530

    Time: 1007 ms (5.06%) | Memory: 227 MB (0.00%) - LeetSync

---
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
@@ -0,0 +1,37 @@
+#define ll long long int 
+const int MOD = 1e9+7;
+class Solution {
+public:
+    int countTrapezoids(vector<vector<int>>& points) {
+        vector<ll> corPoints;
+        vector<ll> pref;
+        unordered_map<int, ll> yCor;
+        for(auto a : points){
+            yCor[a[1]]++;
+        }
+        for(auto a: yCor){
+            ll n = a.second;
+            n--;
+            ll score = n * (n+1)/2;
+            corPoints.push_back(score);
+        }
+        sort( corPoints.begin(),  corPoints.end());
+        for(auto a:  corPoints) cout << a << " ";
+        cout << endl;
+        pref.resize(corPoints.size(), 0);
+        for(int i = 0; i < corPoints.size(); i++){
+            pref[i] = corPoints[i];
+            pref[i] += (i == 0 ? 0ll : pref[i-1]);
+        }
+        for(auto a: pref) cout << a << " ";
+
+        ll ans = 0;
+        for(int i = pref.size()-2; i >= 0; i--){
+            ll a = pref[i] % MOD;
+            ll b = corPoints[i+1]%MOD;
+            ll mul = (a * b) % MOD;
+            ans = (ans + mul) % MOD;
+        }
+        return ans;
+    }
+};
