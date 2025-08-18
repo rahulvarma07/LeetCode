294bf160b27b635cb53286ad1961de0d10d7e973
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2025-08-18 14:50:41 +0530

    Time: 675 ms (18.98%) | Memory: 295.6 MB (17.47%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) 
-    {
-        priority_queue<int>ans;
-        for(auto a:nums)
-        {
-            ans.push(a);
-        } 
-        nums.clear();
-        while(! ans.empty())
-        {
-            nums.push_back(ans.top());
-            ans.pop();
+    void helper(int low, int mid, int high, vector<int> &arr){
+        int left = low, right = mid+1;
+        vector<int> temp;
+        while(left <= mid && right <= high){
+            if(arr[left] <= arr[right]){
+                temp.push_back(arr[left]);
+                left++;
+            }else{
+                temp.push_back(arr[right]);
+                right++;
+            }
         }
-        reverse(nums.begin(),nums.end());
-        return nums;  
+        while(left <= mid){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        while(right <= high){
+            temp.push_back(arr[right]);
+            right++;
+        }
+        for(int i = high; i >= low; i--){
+            arr[i] = temp.back();
+            temp.pop_back();
+        }
+    }
+    void mergeSort(int left, int right, vector<int>&arr){
+        if(left == right) return;
+        int mid = (left + right)/2;
+        mergeSort(left, mid, arr);
+        mergeSort(mid+1, right, arr);
+        helper(left, mid, right, arr);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(0, nums.size()-1, nums);
+        return nums;
     }
 };
