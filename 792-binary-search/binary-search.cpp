f073b354b48c4e45e65339fb495a950b2f630898
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2026-03-16 17:31:02 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (45.08%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-    int BinSer(vector<int>nums, int target, int l, int h){
-        if(l > h) return -1;
-        int mid = (h + l)/2;
-        if(nums[mid] == target) return mid;
-        else if(nums[mid] > target){
-            return BinSer(nums, target, l , mid - 1);
-        }
-        else{
-            return BinSer(nums, target,mid+1, h);
-        }
-    }
     int search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size()-1;
-        int ans = BinSer(nums, target, low, high);
-        return ans;
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
     }
 };
