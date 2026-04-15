bbe220f44bbe5793d9b601ae7d1300f4a0cbf600
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2026-04-15 09:23:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (79.92%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) 
-    {
-        int n = nums.size();
-        vector<int>pref(n, 0);
-        vector<int>suff(n, 0);
-        vector<int>ans(n, 0);
-        pref[0] = nums[0];
-
-        for(int i = 1; i < n; i++){
-            pref[i] = pref[i-1] * nums[i];
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int mul = 1, cnt = 0;
+        vector<int> ans(nums.size(), 0);
+        for(auto a : nums) {
+            if(a == 0) {
+                cnt++;
+                continue;
+            }
+            mul *= a;
         }
-        
-        suff[n-1] = nums[n-1];
-        for(int i = n-2; i >= 0; i--){
-            suff[i] = nums[i] * suff[i+1];
-        }
-        
-        ans[0] = suff[1];
-        ans[n-1] = pref[n-2];
-        for(int i = 1; i < n-1; i++){
-            ans[i] = pref[i-1] * suff[i+1];
+        if(cnt > 1) return ans;
+        for(int i = 0; i < nums.size(); i++) {
+            if(cnt == 0) {
+                ans[i] = mul/nums[i];
+            }else {
+                if(nums[i] == 0) {
+                    ans[i] = mul;
+                }
+            }
         }
         return ans;
     }
