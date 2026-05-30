c42803ce1dc593a5e9d7d8581e7f8ae905f187f6
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2026-05-30 15:38:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (7.44%) - LeetSync

---
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int ans = INT_MAX;
+        for(int i = 0; i < position.size(); i++) {
+            int cp = position[i], cost = 0;
+            for(int j = 0; j < position.size(); j++) {
+                int pp = position[j];
+                if(cp != pp) {
+                    if(abs(pp-cp) % 2 != 0) cost++;
+                }
+            }
+            ans = min(ans, cost);
+        }
+        return ans;
+    }
+};
