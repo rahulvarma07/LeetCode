bb06d66e82f7f14a04e87d726f4d6110575eca1b
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2026-06-13 10:00:02 +0530

    Time: 6 ms (50.51%) | Memory: 7.7 MB (88.01%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
-    int fin(int n)
-    {
-        if(n <= 1) return n;
-        return fin(n - 1) + fin(n - 2);
-    }
     int fib(int n) {
-       int ans =  fin(n);
-       return ans;
+       if(n <= 1) return n;
+       int firstTerm = fib(n-1);
+       int secondTerm = fib(n-2);
+       return firstTerm+secondTerm; 
     }
 };
