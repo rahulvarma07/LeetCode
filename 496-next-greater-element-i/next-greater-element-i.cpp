7e1106e514d588d7d25f5e07c1066959af932855
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2026-06-16 14:28:27 +0530

    Time: 7 ms (10.97%) | Memory: 12.3 MB (92.30%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums2.size();
-        stack<int> monotonic;
-        unordered_map<int, int> myMap;
-        monotonic.push(nums2[n-1]);
-        myMap[nums2[n-1]] = -1;
-        for(int i = n-2; i >= 0; i--){
-            while(monotonic.size() > 0 && monotonic.top() < nums2[i]){
-                monotonic.pop();
+        int n1 = nums1.size(), n2 = nums2.size();
+        stack<int> st;
+        st.push(nums2[n2-1]);
+        vector<int> ans(n1, -1);
+        vector<int> nxt(n2, -1);
+        for(int i = n2-2; i >= 0; i--) {
+            while(!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
             }
-            (monotonic.size() > 0 ? myMap[nums2[i]] = monotonic.top() : myMap[nums2[i]] = -1);
-            monotonic.push(nums2[i]);
+            if(!st.empty()) {
+                nxt[i] = st.top();
+            }
+            st.push(nums2[i]);
         }
-        vector<int> ans(nums1.size(), 0);
-        for(int i = 0; i < ans.size(); i++){
-            ans[i] = myMap[nums1[i]];
+        for(int i = 0; i < n1; i++) {
+            for(int j = 0; j < n2; j++) {
+                if(nums1[i] == nums2[j]) {
+                    ans[i] = nxt[j];
+                }
+            }
         }
         return ans;
     }
