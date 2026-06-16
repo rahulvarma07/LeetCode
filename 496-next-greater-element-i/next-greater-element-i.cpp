367568a79a115882dfa8a0569510b95364cfd40e
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2026-06-16 14:32:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (37.81%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -5,21 +5,19 @@ public:
         stack<int> st;
         st.push(nums2[n2-1]);
         vector<int> ans(n1, -1);
-        vector<int> nxt(n2, -1);
+        unordered_map<int, int> myMap;
         for(int i = n2-2; i >= 0; i--) {
             while(!st.empty() && st.top() <= nums2[i]) {
                 st.pop();
             }
             if(!st.empty()) {
-                nxt[i] = st.top();
+                myMap[nums2[i]] = st.top();
             }
             st.push(nums2[i]);
         }
         for(int i = 0; i < n1; i++) {
-            for(int j = 0; j < n2; j++) {
-                if(nums1[i] == nums2[j]) {
-                    ans[i] = nxt[j];
-                }
+            if(myMap.find(nums1[i]) != myMap.end()) {
+                ans[i] = myMap[nums1[i]];
             }
         }
         return ans;
