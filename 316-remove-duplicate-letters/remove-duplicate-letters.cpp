a02ec79a64f9dface109dedcbf8d54e8ab431f99
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2026-07-21 11:10:26 +0530

    Time: 1 ms (30.74%) | Memory: 9.6 MB (38.62%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char, int> lastInd;
+        for(int i = 0; i < s.size(); i++) {
+            lastInd[s[i]] = i;
+        }
+        vector<bool> isVisited(26, false);
+        stack<char> mSt;
+        for(int i = 0; i < s.size(); i++) {
+            if(isVisited[s[i]-'a']) continue;
+            while(!mSt.empty() && (mSt.top() > s[i] && lastInd[mSt.top()] > i)) {
+                isVisited[mSt.top()-'a'] = false;
+                mSt.pop();
+            }
+            isVisited[s[i]-'a'] = true;
+            mSt.push(s[i]);
+        }
+        string ans = "";
+        while(!mSt.empty()) {
+            ans += mSt.top();
+            mSt.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
