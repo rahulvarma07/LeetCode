dc5a8d33edfba1ff723ccf76235d771d2cb6d484
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2026-08-05 16:25:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (46.81%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
     int minLength(string s) {
-        string str = s;
-        int ans = INT_MAX;
-        while(true){
-            bool check = false;
-            for(int i = 1; i < s.size(); i++){
-                if(s[i-1] == 'A' && s[i] == 'B'){
-                    s[i] = '*';
-                    s[i-1] = '*';
-                    check = true;
-                }
-                else if(s[i] == 'D' && s[i-1] == 'C'){
-                    check = true;
-                    s[i] = '*';
-                    s[i-1] = '*';
-                }
+        stack<char> st;
+        // "BCD"
+        for(int i = 0; i < s.size(); i++) {
+            if(!st.empty() && s[i] == 'B' && st.top() == 'A') {
+                st.pop();
+            }else if(!st.empty() && s[i] == 'D' &&  st.top() == 'C') {
+                st.pop();
+            }else {
+                st.push(s[i]);
             }
-            if(check == false) break;
-            str = "";
-            for(int i = 0; i < s.size(); i++){
-                if(s[i] != '*') str += s[i];
-            }
-            s = str;
         }
-        cout << s << " ";
-        return s.size();
+        return st.size();
     }
 };
