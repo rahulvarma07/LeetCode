3fc597f8f23f280141a77f6e1d24b40a8da93b85
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2026-08-19 17:56:46 +0530

    Time: 67 ms (39.53%) | Memory: 92 MB (10.47%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    bool checkRange(int strt, int end, unordered_set<int> & mySet) {
+        for(int i = strt; i <= end; i++) {
+            if(mySet.count(i)) return false;
+        }
+        return true;
+    }
+
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
         unordered_map<int, unordered_set<int>> seats;
         for(auto &a : reservedSeats) {
@@ -7,24 +14,7 @@ public:
         }
         long long int ans = (n - seats.size())*2;
         for(auto &a: seats) {
-            bool p1 = true, p2 = true, p3 = true;
-            // p1 && p3 == > 2
-            // p1 || p2 || p3
-            for(int i = 2; i <= 5; i++) {
-                if((a.second.count(i))) {
-                    p1 = false;
-                }
-            }
-            for(int i = 4; i <= 7; i++) {
-                if((a.second.count(i))) {
-                    p2 = false;
-                }
-            }
-            for(int i = 6; i <= 9; i++) {
-                if((a.second.count(i))) {
-                    p3 = false;
-                }
-            }
+            bool p1 = checkRange(2, 5, a.second), p2 = checkRange(4, 7, a.second), p3 = checkRange(6, 9, a.second);
             if(p1 && p3) ans = ans + 2;
             else if(p1 || p2 || p3) ans = ans + 1;
         }
